b04d317c364d4e66f042c44459f6e8bcefaff96e
David.goerig <David.goerig@epitech.eu>
2020-02-04 21:20:01 +0100

    godav: refactoring

--- GeneticAlgoBool.java
+++ GeneticAlgoBool.java
@@ -16,11 +16,13 @@ class GeneticAlgoBool {
 	private double crossRate;
 
 
-	private boolean[][] population;
-	private double[][] fitnessValues;
-	private boolean[][] newPopulation;
-	private boolean isAcceptable;
-	private int indexOfAcceptableSolution;
+	private boolean[][] samp;
+	private boolean[][] sampTemp;
+	private double[][] valFitArray;
+
+
+	private boolean stopCondition;
+	private int solIndexx;
 	private long startT;
 
 	// Intialize the different variables
@@ -33,102 +35,102 @@ class GeneticAlgoBool {
 		this.crossRate = crossRate;
 
 		this.startT = startT;
-		population = getInitialPopulation();
-		fitnessValues = new double[popSize][2];
-		newPopulation = new boolean[popSize][solSize];
-		isAcceptable = false;
+		samp = createSamplesArray();
+		valFitArray = new double[popSize][2];
+		sampTemp = new boolean[popSize][solSize];
+		stopCondition = false;
 	}
 
 	/**
 	 * Constructs a random population with the size defined by the class.
 	 * @return The new random population
 	 */
-	private boolean[][] getInitialPopulation() {
-		boolean[][] population = new boolean[popSize][solSize];
+	private boolean[][] createSamplesArray() {
+		boolean[][] samp = new boolean[popSize][solSize];
 
 		for (int i = 0; i < popSize; i++) {
 			for (int j = 0; j < solSize; j++) {
-				population[i][j] = Math.random() > 0.5 ? true : false;
+				samp[i][j] = Math.random() > 0.5 ? true : false;
 			}
 		}
-		return population;
+		return samp;
 	}
 
 	/**
 	 * Updates the fitness value of the different solutions.
 	 * @return The minimum fitness value found.
 	 */
-	private void updateFitnessValuesCurrentPopulation() {
-		double[] bestFitness = null;
-		int index = -1;
+	private void computeFitnessOnSample() {
+		double[] topFindedFitness = null;
+		int solutionIndex = -1;
 
 		for (int i = 0; i < popSize; i++) {
-			fitnessValues[i] = Assess.getTest2(population[i]);
-			if (bestFitness == null) {
-				bestFitness = fitnessValues[i];
-				index = i;
-			} else if (fitnessValues[i][0] <= 500) {
-				if (bestFitness[0] > 500) {
-					bestFitness = fitnessValues[i];
-					index = i;
-				} else if (fitnessValues[i][1] > bestFitness[1]) {
-					bestFitness = fitnessValues[i];
-					index = i;
+			valFitArray[i] = Assess.getTest2(samp[i]);
+			if (topFindedFitness == null) {
+				topFindedFitness = valFitArray[i];
+				solutionIndex = i;
+			} else if (valFitArray[i][0] <= 500) {
+				if (topFindedFitness[0] > 500) {
+					topFindedFitness = valFitArray[i];
+					solutionIndex = i;
+				} else if (valFitArray[i][1] > topFindedFitness[1]) {
+					topFindedFitness = valFitArray[i];
+					solutionIndex = i;
 				}
 			} else {
-				if (fitnessValues[i][0] < bestFitness[0]) {
-					bestFitness = fitnessValues[i];
-					index = i;
+				if (valFitArray[i][0] < topFindedFitness[0]) {
+					topFindedFitness = valFitArray[i];
+					solutionIndex = i;
 				}
 			}
 		}
-		indexOfAcceptableSolution = index;
+		solIndexx = solutionIndex;
 	}
 
 	/**
 	 * Tournament selection on the current population with the defined parameter by the class.
 	 * @return The selected solution
 	 */
-	private boolean[] tournamentSelection() {
-		double[] bestFitness = null;
-		int index = -1;
-		int tmpIdx;
+	private boolean[] selection() {
+		double[] topFindedFitness = null;
+		int solutionIndex = -1;
+		int comparingIndex;
 
 		for (int i = 0; i < nbrSampleSelection; i++) {
-			tmpIdx = (int)(Math.random() * popSize);
-			if (tmpIdx >= popSize) {
-				tmpIdx = popSize - 1;
+			comparingIndex = (int)(Math.random() * popSize);
+			if (comparingIndex >= popSize) {
+				comparingIndex = popSize - 1;
 			}
-			if (bestFitness == null) {
-				bestFitness = fitnessValues[tmpIdx];
-				index = tmpIdx;
-			} else if (fitnessValues[tmpIdx][0] <= 500) {
-				if (bestFitness[0] > 500) {
-					bestFitness = fitnessValues[tmpIdx];
-					index = tmpIdx;
-				} else if (fitnessValues[tmpIdx][1] > bestFitness[1]) {
-					bestFitness = fitnessValues[tmpIdx];
-					index = tmpIdx;
+			if (topFindedFitness == null) {
+				topFindedFitness = valFitArray[comparingIndex];
+				solutionIndex = comparingIndex;
+			} else if (valFitArray[comparingIndex][0] <= 500) {
+				if (topFindedFitness[0] > 500) {
+					topFindedFitness = valFitArray[comparingIndex];
+					solutionIndex = comparingIndex;
+				} else if (valFitArray[comparingIndex][1] > topFindedFitness[1]) {
+					topFindedFitness = valFitArray[comparingIndex];
+					solutionIndex = comparingIndex;
 				}
 			} else {
-				if (fitnessValues[tmpIdx][0] < bestFitness[0]) {
-					bestFitness = fitnessValues[tmpIdx];
-					index = tmpIdx;
+				if (valFitArray[comparingIndex][0] < topFindedFitness[0]) {
+					topFindedFitness = valFitArray[comparingIndex];
+					solutionIndex = comparingIndex;
 				}
 			}
 		}
 		//System.out.println(bestFitness[0]);
-		return population[index].clone();
+		return samp[solutionIndex].clone();
 	}
 
 	/**
 	 * Computes mutation on the new population in order to change the results.
 	 * The mutation is made on the solution and stored in it, make sure it is a copy of your original solution
 	 */
-	private void mutation(boolean[] solution) {
-		for (int i = 0; i < solution.length; i++) {
+	private void mutOnCandidat(boolean[] indiviu) {
+		for (int i = 0; i < indiviu.length; i++) {
 			if (Math.random() * 100 % 100 < mutRate) {
-				solution[i] = !solution[i];
+				indiviu[i] = !indiviu[i];
 			}
 		}
 	}
@@ -137,19 +139,19 @@ class GeneticAlgoBool {
 	 * Crossover throughs the current population with the defined rate
 	 * The crossover is made on the parent1 and stored in it, make sure it is a copy of your original solution
 	 */
-	private void crossover(boolean[] parent1, boolean[] parent2, int nbCrossoverPoints) {
+	private void crossOnCandidat(boolean[] to_cross_1, boolean[] to_cross_2, int cross_pt_nbr) {
 		if (Math.random() * 100 % 100 < crossRate) {
-			int rdmValue = (int)(Math.random() * nbCrossoverPoints);
-			int nbPoints = rdmValue >= 1 ? rdmValue : 1;
+			int random = (int)(Math.random() * cross_pt_nbr);
+			int nbPoints = random >= 1 ? random : 1;
 			boolean odd = Math.random() > 0.5 ? true : false;
 
 			for (int i = 0; i < nbPoints; i++) {
 				for (int j = (int)((solSize / (float) nbPoints) * i);
 					 j < (int)((solSize / (float) nbPoints) * (i + 1)); j++) {
 					if (i % 2 == 0 && odd) {
-						parent1[j] = parent2[j];
+						to_cross_1[j] = to_cross_2[j];
 					} else if (i % 2 != 0 && !odd) {
-						parent1[j] = parent2[j];
+						to_cross_1[j] = to_cross_2[j];
 					}
 				}
 			}
@@ -162,24 +164,24 @@ class GeneticAlgoBool {
 	 * "mutation"
 	 * "crossover"
 	 */
-	private void getNewPopulation() {
+	private void createConcurrentSample() {
 		int tmpIdx;
 		boolean[][] tmp;
 
 		for (int i = 0; i < popSize; i++) {
-			boolean[] solution = tournamentSelection();
+			boolean[] solution = selection();
 
 			tmpIdx = (int)(Math.random() * popSize);
 			if (tmpIdx >= popSize) {
 				tmpIdx = popSize - 1;
 			}
-			mutation(solution);
-			crossover(solution, population[tmpIdx], nbCrossPoint);
-			newPopulation[i] = solution;
+			mutOnCandidat(solution);
+			crossOnCandidat(solution, samp[tmpIdx], nbCrossPoint);
+			sampTemp[i] = solution;
 		}
-		tmp = population;
-		population = newPopulation;
-		newPopulation = tmp;
+		tmp = samp;
+		samp = sampTemp;
+		sampTemp = tmp;
 	}
 
 	/**
@@ -189,15 +191,15 @@ class GeneticAlgoBool {
 	public boolean[] getSol() {
 		long tmpT;
 
-		updateFitnessValuesCurrentPopulation();
-		while (isAcceptable == false) {
-			getNewPopulation();
-			updateFitnessValuesCurrentPopulation();
+		computeFitnessOnSample();
+		while (stopCondition == false) {
+			createConcurrentSample();
+			computeFitnessOnSample();
 			tmpT = System.currentTimeMillis();
 			if ((tmpT - startT) / 1000.0 > 14) {
-				isAcceptable = true;
+				stopCondition = true;
 			}
 		}
-		return population[indexOfAcceptableSolution];
+		return samp[solIndexx];
 	}
 }
